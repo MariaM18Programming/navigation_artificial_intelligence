d669d138e21679e2b456bfd9e962d162229cd4b8
MariaM18Programming <96386495+MariaM18Programming@users.noreply.github.com>
2024-06-24 15:26:17 +0300

    Add files via upload

---
+++ dijkstra.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+const long long INF = 1e18;
+vector<vector<pair<int, int>>> G;
+vector<long long> dist;
+vector<int> pred;
+
+void dijkstra(int start) {
+    dist[start] = 0;
+    pred[start] = -1;
+    set<pair<long long, int>> pq;
+    pq.insert({0, start});
+    while (!pq.empty()) {
+        int u = pq.begin()->second;
+        pq.erase(pq.begin());
+        for (auto [v, wt] : G[u]) {
+            if (dist[u] + wt < dist[v]) {
+                pq.erase({dist[v], v});
+                dist[v] = dist[u] + wt;
+                pred[v] = u;
+                pq.insert({dist[v], v});
+            }
+        }
+    }
+}
+
+int main() {
+    int w, h;
+    cin >> w >> h;
+    vector<vector<int>> a(w, vector<int>(h));
+    for (int i = 0; i < w; i++) {
+        for (int j = 0; j < h; j++) {
+            char c;
+            cin >> c;
+            if (c == '.') {
+                a[i][j] = 1;
+            }
+        }
+    }
+    G.resize(w * h);
+    dist.resize(w * h, INF);
+    pred.resize(w * h);
+    vector<pair<int, int>> sosedi = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    for (int i = 0; i < w; i++) {
+        for (int j = 0; j < h; j++) {
+            if (!a[i][j]) {
+                continue;
+            }
+            for (auto e : sosedi) {
+                int x = i + e.first, y = j + e.second;
+                if (x >= 0 && x < w && y >= 0 && y < h && a[x][y]) {
+                    G[i * h + j].push_back({x * h + y, 1});
+                    G[x * h + y].push_back({i * h + j, 1});
+                }
+            }
+        }
+    }
+    int st_x, st_y, f_x, f_y;
+    cin >> st_x >> st_y >> f_x >> f_y;
+    int start = st_x * h + st_y;
+    dijkstra(start);
+    int pos = f_x * h + f_y;
+    vector<int> ans;
+    while (pos != -1) {
+        ans.push_back(pos);
+        pos = pred[pos];
+    }
+    reverse(ans.begin(), ans.end());
+    cout << "Расстояние: " << dist[f_x * h + f_y] << "\n";
+    cout << "Путь: \n";
+    for (auto x : ans) {
+        cout << x / h << " " << x % h << "\n";
+    }
+    return 0;
+}
